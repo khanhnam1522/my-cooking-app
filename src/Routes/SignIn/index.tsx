6a619d9e2e67c3407a635d9daa4407801f6d3191
namtang <khanhnam1522@gmail.com>
2021-07-05 01:35:48 -0700

    using styled components

--- src/Routes/SignIn/index.tsx
+++ src/Routes/SignIn/index.tsx
@@ -5,6 +5,10 @@ import {
   NavIcon,
   NavbarContainer,
   BackgroundGradient,
+  FormContainer,
+  Form,
+  InputForm,
+  SubmitButton,
 } from "./styles";
 
 const SignIn: React.FC = () => {
@@ -21,63 +25,14 @@ const SignIn: React.FC = () => {
           </NavbarContainer>
 
           {/* Form */}
-          <div
-            style={{
-              position: "absolute",
-              top: "30%",
-              zIndex: 1,
-              maxWidth: "400px",
-              padding: "70px",
-              marginLeft: "auto",
-              marginRight: "auto",
-              background: "rgba(0, 0, 0, 0.85)",
-              left: 0,
-              right: 0,
-            }}
-          >
-            <form style={{ display: "grid", flexDirection: "column" }}>
+          <FormContainer>
+            <Form>
               <h1 style={{ marginBottom: "14px", color: "#fff" }}>Sign In</h1>
-              <input
-                placeholder="Email"
-                type="email"
-                style={{
-                  outlineWidth: 0,
-                  height: "40px",
-                  marginBottom: "14px",
-                  borderRadius: "5px",
-                  border: "none",
-                  padding: "5px 15px",
-                }}
-              />
-              <input
-                placeholder="Password"
-                type="password"
-                style={{
-                  outlineWidth: 0,
-                  height: "40px",
-                  marginBottom: "14px",
-                  borderRadius: "5px",
-                  border: "none",
-                  padding: "5px 15px",
-                }}
-              />
-              <button
-                type="submit"
-                style={{
-                  padding: "16px 20px",
-                  //   fontSize: "1rem",
-                  color: "#fff",
-                  borderRadius: "5px",
-                  backgroundColor: "#e50914",
-                  cursor: "pointer",
-                  border: "none",
-                  marginTop: "20px",
-                }}
-              >
-                Sign In
-              </button>
-            </form>
-          </div>
+              <InputForm placeholder="Email" type="email" />
+              <InputForm placeholder="Password" type="password" />
+              <SubmitButton type="submit">Sign In</SubmitButton>
+            </Form>
+          </FormContainer>
         </BackgroundGradient>
       </LoginContainer>
     </div>
